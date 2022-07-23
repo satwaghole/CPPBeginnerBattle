27283ac6638239b37b1bf33fb49a074802a399c3
HumbleSwagger <39756792+satwaghole@users.noreply.github.com>
2022-07-23 15:39:09 +0530

    string breaking with strtok
    have not used stringstream class, used tradition c way with strtok() to break the string

---
+++ HR_string_breaking_by_comma.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+// problem - https://www.hackerrank.com/challenges/c-tutorial-stringstream/problem?isFullScreen=true
+// i/p- "23,4,56"
+/* O/p- 
+23
+4
+56
+*/
+
+vector<int> parseInts(string str) {
+	// Complete this function
+	 //  stringstream ss(str) ;
+
+    char *charPtr = strtok(const_cast<char*>(str.c_str()),",");
+    vector<int> vInt;
+    string word;
+    while(charPtr!=NULL)
+    {
+        int num = stoi(charPtr);
+        vInt.push_back(num);
+        charPtr = strtok (NULL, ",");
+    }
+    return vInt;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    return 0;
+}
