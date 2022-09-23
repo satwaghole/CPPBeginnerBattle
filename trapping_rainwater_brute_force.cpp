9d1915815d4a837de7b58fb0934bd26124f75c0d
HumbleSwagger <39756792+satwaghole@users.noreply.github.com>
2022-09-24 02:14:32 +0530

    very basic approach
    O(n^2)--worst TC

---
+++ trapping_rainwater_brute_force.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+/*
+	Leetcode - rainwater trapping problem
+	i/p - 11
+		  0 1 0 2 1 0 3 1 0 1 2
+    o/p- 8
+*/
+
+int max_rainwater_trapped(vector<int> &height)
+{
+	int maxLeft, maxRight;
+	int water_stored = 0 , final_water_stored=0;
+
+	for(int i=0; i< height.size(); ++i)
+	{
+		maxLeft = 0, maxRight = 0;
+
+		for(int j= i-1; j>=0; --j)
+		{ 
+			int mLeft = height[j];
+			maxLeft = (mLeft > maxLeft) ? mLeft : maxLeft;
+		}
+		for(int k= i+1; k<height.size(); ++k)
+		{
+			int mRight = height[k];
+			maxRight = (mRight > maxRight) ? mRight : maxRight;
+		}
+
+		water_stored = min(maxLeft, maxRight) - height[i];
+
+		if(water_stored > 0)
+			final_water_stored += water_stored ;
+	}
+
+	return final_water_stored;
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputf.in","r",stdin);
+	freopen("outputf.out","w",stdout);
+	#endif
+
+	int total;
+	cin>>total;
+
+	vector<int> vec;
+
+	for(int i=0; i< total; ++i)
+	{
+		int temp;
+		cin>>temp;
+		vec.push_back(temp);
+	}
+
+	cout<<max_rainwater_trapped(vec);
+
+	return 0;
+}
