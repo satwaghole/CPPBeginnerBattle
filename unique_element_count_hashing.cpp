334ebb7c16b457a773fc865ccf10b870ae2c5c08
HumbleSwagger <39756792+satwaghole@users.noreply.github.com>
2022-12-17 01:12:17 +0530

    Update unique_element_count_hashing.cpp

--- unique_element_count_hashing.cpp
+++ unique_element_count_hashing.cpp
@@ -11,15 +11,21 @@ int main()
 	vector<int> A={5, 8, 15, 3, 20, 2};
 	int flag=0;
 	
-	 for (int i=0; i<A.size(); i++)
+    int max= *max_element(A.begin(), A.end());
+
+    for(int k=0;k<N; ++k)
     {
-    	hsh[A[i]]++;
-    	
-		}
-    for (int j=0; j<A.size(); j++)
+        hsh[k]=0;
+    }
+    
+     for(int i=0; i<A.size(); i++)
     {
-       if(hsh[A[j]] == 1)
-					flag++;
-		}
-    cout<<flag;
+        hsh[A[i]]++;
+    }
+    for (int j=0; j<=max; j++)
+    {
+       if(hsh[j] == 1)
+            flag++;
+    }
+    return flag;
 }
